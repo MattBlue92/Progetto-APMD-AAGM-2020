c5641efe6d5922a324c065c976fccb083704b837
Matteo <matteo.ghera@stud.unifi.it>
2020-06-03 16:36:09 +0200

    add R network

--- scripts/src/ClosenessCentrality.py
+++ scripts/src/ClosenessCentrality.py
@@ -10,10 +10,12 @@ class ClosenessCentrality():
 
     def closenessUsingUtilities(self, graph, networkx=False):
         closeness = {}
-        for v in graph.nodes():
+        nodes=graph.nodes._nodes
+        len_nodes=len(nodes)
+        for v in nodes:
             fv = 0
             count=0
-            for w in graph.nodes():
+            for w in nodes:
                 if nx.has_path(graph, v, w):
                     if v!=w:
                         path = nx.shortest_path(graph, v, w)
@@ -21,7 +23,7 @@ class ClosenessCentrality():
                     count=count+1
             if fv!=0:
                 if not networkx:
-                    closeness[v]=(len(graph)-1)/fv
+                    closeness[v]=(len_nodes-1)/fv
                 else:
                     closeness[v] = (count-1)/fv
             else:
@@ -31,16 +33,20 @@ class ClosenessCentrality():
 
     def closenessUsingBFS(self, graph, networkx=False):
         closeness = {}
-        for v in graph.nodes():
+        nodes=graph.nodes._nodes
+        len_nodes=len(nodes)
+        for v in nodes:
             bfsTree = nx.bfs_tree(graph, v)
+            bfs_nodes = bfsTree.nodes._nodes
+            len_bfs_nodes = len(bfs_nodes)
             fv = 0
-            for w in bfsTree.nodes():
+            for w in bfs_nodes:
                 fv = fv + self.distance(bfsTree, v, w)
             if fv!=0:
                 if not networkx:
-                    closeness[v] = (len(graph)-1)/fv
+                    closeness[v] = (len_nodes-1)/fv
                 else:
-                    closeness[v] = (len(bfsTree) - 1) / fv
+                    closeness[v] = (len_bfs_nodes - 1) / fv
             else:
                 closeness[v] = 0
 
@@ -51,11 +57,12 @@ class ClosenessCentrality():
         k = int(math.log(n,10) / np.power(epsilon, 2))
         closeness = {}
         i = 0
-        for u in graph.nodes():
+        nodes = graph.nodes._nodes
+        for u in nodes:
             fv = 0
-            nodes = np.random.choice(list(graph.nodes()), size= k, replace= False)
+            sample_nodes = np.random.choice(list(nodes), size= k, replace= False)
             i = i+1
-            for w in nodes:
+            for w in sample_nodes:
                 bfsTree = nx.bfs_tree(graph, w)
                 distance = self.distance(bfsTree, w, u)
                 fv = fv+distance*(n/(k*(n-1)))
@@ -88,7 +95,8 @@ class ClosenessCentrality():
         distance = 0
         bfsTree = bfsTree.reverse(copy = True)
         while v!=w:
-            if w  in list(bfsTree.nodes()):
+            bfsTreeNodes=bfsTree.nodes._nodes
+            if w  in bfsTreeNodes:
                 neighbors = list(bfsTree[w])
                 w = neighbors.pop()
                 distance = distance+1
